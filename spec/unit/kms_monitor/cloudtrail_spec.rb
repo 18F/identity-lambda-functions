b17010982ec068439d390256dc7afea192ee43dd
Mark Ryan <mark-from-usds@users.noreply.github.com>
2019-04-05 16:48:57 -0400

    cleanup a lot of nonsense

--- spec/unit/kms_monitor/cloudtrail_spec.rb
+++ spec/unit/kms_monitor/cloudtrail_spec.rb
@@ -18,24 +18,20 @@ class FakeDynamoClient
   end
 
   def get_item(params)
-    puts "get_item: #{params}"
     object_key = object_key(params[:table_name], params[:key])
-    puts "object_key: #{object_key}"
     inner_client.stub_responses(:get_item, {item: objects[object_key]})
     inner_client.get_item(params)
   end
 
   def put_item(params)
-    puts "put_item: #{params}"
     item = params[:item]
     object_key = object_key(params[:table_name], item)
-    puts "object_key: #{object_key}"
     objects[object_key] = item
   end
 end
 
 RSpec.describe IdentityKMSMonitor::CloudTrailToDynamoHandler do
-  describe 'something in the cloudtrail class' do
+  describe 'the process method' do
     it 'writes a match to an existing DB entry' do
       ENV['DDB_TABLE'] = 'fake_table'
       fake_dynamo = FakeDynamoClient.new
@@ -50,7 +46,6 @@ RSpec.describe IdentityKMSMonitor::CloudTrailToDynamoHandler do
         {:table_name=>"fake_table",
          :key=>{"UUID"=>"ad891a65-4560-4669-b422-b61cd5f9c861-password-digest",
                 "Timestamp"=>"2019-03-08T13:32:07Z"}})
-      puts "final_entry: ", final_entry
       expect(final_entry.item['Correlated']).to eq '1'
     end
   end
