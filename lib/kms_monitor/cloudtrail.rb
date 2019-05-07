8cc610f5b26ec2ba8683994faec8cef910e67ea4
Mark Ryan <34074974+mark-from-usds@users.noreply.github.com>
2019-05-07 10:35:57 -0400

    Add error notification and retry logic to the CloudTrail lambda (#7)
    * Add requeuing and SNS notifications to the CloudTrail Lambda

--- lib/kms_monitor/cloudtrail.rb
+++ lib/kms_monitor/cloudtrail.rb
@@ -2,6 +2,8 @@
 
 require 'logger'
 require 'aws-sdk-dynamodb'
+require 'aws-sdk-sns'
+require 'aws-sdk-sqs'
 
 module IdentityKMSMonitor
 
@@ -12,11 +14,18 @@ module IdentityKMSMonitor
 
     Functions.register_handler(self, 'cloudtrail-to-dynamo')
 
-    def initialize(log_level: Logger::INFO, dry_run: true, dynamo: nil)
+    def initialize(log_level: Logger::INFO, dry_run: true, dynamo: nil,
+                   sns: nil, sqs: nil)
       super(log_level: log_level, dry_run: dry_run)
 
       begin
         @dynamo = dynamo || Aws::DynamoDB::Client.new
+        @sns = sns || Aws::SNS::Client.new
+        @sqs = sqs || Aws::SQS::Client.new
+        @sns_event_topic_arn = ENV.fetch('SNS_EVENT_TOPIC_ARN')
+        @dynamodb_table_name = ENV.fetch('DDB_TABLE')
+        @retention_days = Integer(ENV.fetch('RETENTION_DAYS'))
+        @cloudtrail_queue_url = ENV.fetch('CT_QUEUE_URL')
       rescue StandardError
         log.error('Failed to create DynamoDB client. Do you have AWS creds?')
         raise
@@ -56,6 +65,7 @@ module IdentityKMSMonitor
     # @param [Hash] record
     def process_record(record)
       body = JSON.parse(record.fetch('body'))
+      log.info("record body: #{body.inspect}")
 
       ctevent = CloudTrailEvent.new
       timestamp = Time.parse(body.fetch('detail').fetch('eventTime')).utc
@@ -66,46 +76,115 @@ module IdentityKMSMonitor
       ctevent.context = request_parameters.fetch(
         'encryptionContext').fetch('context')
 
-      # get matching record
-      apprecord = get_app_record(ctevent.get_key, ctevent.timestamp)
-      log.info "apprecord result: #{apprecord.to_h}"
+      dbrecord = get_db_record(ctevent.get_key, ctevent.timestamp)
 
-      if apprecord&.key?('CWData')
+      # calculate retry count will not be used if we found the record in table
+      retrycount = get_attribute_value_int(record, 'RetryCount')
+      retrycount += 1
+
+      if dbrecord
+        # found the record in table, update and mark correlated
+        # generate event
+        log.info('Matching CloudWatch event found. Marking correlated in ' +
+                 'database and notifying SNS.')
         insert_into_db(ctevent.get_key, ctevent.timestamp, body,
-                       apprecord['CWData'])
+                       dbrecord.fetch('CWData'), 1)
+        log_event_sns(ctevent, body.fetch('id'), true)
+      elsif retrycount <= 5
+        # put message back on queue
+        log.info('No matching CloudWatch event found. Requeuing this event.')
+        delay = calculate_delay(retrycount)
+        put_message_queue(body, delay, retrycount)
       else
-        log.error 'No CloudWatch data found for this event.'
+        # all retries exhausted, put record in table uncorrelated
+        log.warn('No matching CloudWatch event found and retries exhausted. ' +
+                 'Marking this event uncorrelated in the database and ' +
+                 'notifying SNS.')
+        insert_into_db(ctevent.get_key, ctevent.timestamp, body, '', 0)
+        # generate SNS event to notify of the issue
+        log_event_sns(ctevent, body.fetch('id'), false)
       end
     end
 
-    def get_app_record(uuid, timestamp)
+    def log_event_sns(ctevent, cloudtrail_id, correlated)
+      logentry = LogEntry.new
+      logentry.uuid = ctevent.uuid
+      logentry.operation = 'decrypt'
+      logentry.context = ctevent.context
+      logentry.timestamp = ctevent.timestamp
+      logentry.cloudtrail_id = cloudtrail_id
+      logentry.correlated = correlated
+      logentry.late_correlation = false
+
       begin
-        result = dynamo.get_item(
-          table_name: ENV.fetch('DDB_TABLE'),
-          key: { 'UUID' => uuid,
-                 'Timestamp' => timestamp },
-          consistent_read: false
-                                 )
+        @sns.publish(
+          topic_arn: @sns_event_topic_arn,
+          message: logentry.to_json
+                     )
       rescue Aws::DynamoDB::Errors::ServiceError => error
-        log.error "Failure looking up event: #{error.message}"
+        log.error "Failure publishing to SNS: #{error.inspect}"
         raise
       end
-      log.info "Database query result: #{result}"
+    end
+
+    def put_message_queue(message_body, message_delay, message_retrycount)
+      bodystring = message_body.to_json
+      @sqs.send_message(
+        queue_url: @cloudtrail_queue_url,
+        message_body: bodystring,
+        delay_seconds: message_delay,
+        message_attributes: {
+          'RetryCount' => {
+            string_value: message_retrycount.to_s,
+            data_type: 'Number',
+          },
+        }
+                        )
+    rescue Aws::SQS::Errors::ServiceError => error
+      log.error "Failure publishing to SQS: #{error.inspect}"
+      raise
+    end
+
+    def get_attribute_value_int(record, attribute_key)
+      attributes = record.fetch('messageAttributes', {})
+      log.info("message attributes: #{attributes.inspect}")
+      Integer(attributes.fetch(attribute_key, {})['stringValue'] || 0)
+    end
+
+    def calculate_delay(counter)
+      [900, (counter**2) * 30].min
+    end
+
+    def get_db_record(uuid, timestamp)
+      begin
+        result = dynamo.get_item(
+          table_name: @dynamodb_table_name,
+          key: { 'UUID' => uuid,
+                 'Timestamp' => timestamp, },
+          consistent_read: true
+          )
+      rescue Aws::DynamoDB::Errors::ServiceError => error
+        log.error "Failure looking up event: #{error.inspect}"
+        raise
+      end
+      log.info "Database query result: #{result.inspect}"
       result.item
     end
 
-    def insert_into_db(uuid, timestamp, ctdata, cwdata)
-      table_name = ENV.fetch('DDB_TABLE')
-      ttl = Time.now.utc + 365
+    def insert_into_db(uuid, timestamp, ctdata, cwdata, correlated)
+      table_name = @dynamodb_table_name
+      ttl = Time.now.utc + @retention_days
       ttlstring = ttl.strftime('%Y-%m-%dT%H:%M:%SZ')
       item = {
         'UUID' => uuid,
         'Timestamp' => timestamp,
-        'Correlated' => '1',
+        'Correlated' => correlated.to_s,
         'CTData' => ctdata,
-        'CWData' => cwdata,
         'TimeToExist' => ttlstring,
       }
+      unless cwdata.empty?
+        item['CWData'] = cwdata
+      end
 
       params = {
         table_name: table_name,
@@ -113,10 +192,10 @@ module IdentityKMSMonitor
       }
 
       begin
+        log.info "Writing event with params: #{params.inspect}"
         dynamo.put_item(params)
-        log.info "Added event for user_uuid: #{uuid}"
       rescue Aws::DynamoDB::Errors::ServiceError => error
-        log.info "Failure adding event: #{error.message}"
+        log.info "Failure adding event: #{error.inspect}"
       end
     end
 
@@ -135,6 +214,29 @@ module IdentityKMSMonitor
         uuid: @uuid,
         timestamp: @timestamp,
         context: @context,
+      }
+    end
+
+    def to_json(*options)
+      to_h(*options).to_json(*options)
+    end
+  end
+
+  # Events we write to SNS
+  class LogEntry
+
+    attr_accessor :context, :operation, :uuid, :timestamp, :cloudtrail_id,
+                  :late_correlation, :correlated
+
+    def to_h(_options = {})
+      {
+        uuid: @uuid,
+        operation: @operation,
+        context: @context,
+        timestamp: @timestamp,
+        cloudtrail_id: @cloudtrail_id,
+        correlated: @correlated,
+        late_correlation: @late_correlation,
       }
     end
 
